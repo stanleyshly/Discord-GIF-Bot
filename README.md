fba9b9f4cbb6e7e0a3618c4ea29f9d97e4ce20d6
Stanley Shen <stanleyshenlg@gmail.com>
2022-05-11 20:37:48 -0700

    First Working Version with README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pranav Bot
+
+This is a Discord bot that sends semi-relavent gifs to "spice" up your server discussions just a bit more. 
+
+Inspired by Pranav, of course
+
+## Setup
+1) Install dependencies with ``` pip install -r requirements.txt ```
+2) Create ``` .env ``` file in the same directory
+3) Set up ```.env``` to look something like this:
+```
+DISCORD_TOKEN=DISCORDTOKENGOESHERE
+TENOR_TOKEN=TENORTOKENGOESHERE
+WHITELIST_CHANNEL=["stanley-bot-playground"]
+NUMBER_IMGUR_SEARCH=8
+SPACY_MODEL=en_core_web_trf
+```
+4) Add Discord and Tenor token 
+5) Create array with all channels where bot will reply with GIFs (Warning, Failure to do so correctly) will result in very annoyed ex-friends.
+6) Set ```NUMBER_IMGUR_SEARCH``` to set the max of gifs to search on Imgur each time(increasing this number slows down the bot significantly but allows for a greater variety of GIFs returned)
+7) Set  ```NLP_MODEL``` to use a model that Spacey supports; install and configure the models with instructions here before starting the bot: [Spacy Website](https://spacy.io/models)
+7a) For english, ```en_core_web_trf``` is alright, but if compute limited, use ```en_core_web_sm```
+8) Start bot and enjoy
---
+++ main.py
@@ -0,0 +1,98 @@
+import discord
+import logging
+import spacy
+import json
+import requests
+import random
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import time
+
+load_dotenv()
+
+client = discord.Client()
+logging.basicConfig(
+    level=logging.INFO,
+    filename='bot.log',
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+#uncomment below to print log to console too
+logging.getLogger().addHandler(logging.StreamHandler())
+nlp = spacy.load(os.getenv("NLP_MODEL"))
+
+
+# set the apikey and limit
+apikey = os.getenv("TENOR_TOKEN")
+max_gif_lmt = os.getenv("NUMBER_IMGUR_SEARCH") # max gif to return
+
+
+@client.event
+async def on_ready():
+    logging.info('We have logged in as {0.user}'.format(client)) 
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content: # this checks of message is sent
+        if message.channel.name in os.getenv("WHITELIST_CHANNEL"):
+            logging.info("Message Received at: "+ str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+  " Message is: " +  str(message.content))
+            start_time =  time.time()    
+            doc=nlp((message.content))
+            noun_array = [chunk.text for chunk in doc.noun_chunks]
+            verb_array =  [token.lemma_ for token in doc if token.pos_ == "VERB"]
+            end_time = time.time()    
+            logging.info("NLP Finished in: "+ str(end_time-start_time)+ " sec")
+            search_term_big_array = verb_array + noun_array
+            logging.info("All possible search terms are "+ str(search_term_big_array))
+            start_time = end_time
+            
+            if search_term_big_array: # check if array is empty
+                while True:
+                    # our test search
+                    search_term = search_term_big_array[random.randint(0,len(search_term_big_array)-1)]
+                    logging.info("Search Term is: "+ search_term)
+                    logging.info("Imjur Search Finished in: "+ str(end_time-start_time)+ " sec")
+                    # get the top 8 GIFs for the search term
+                    r = requests.get(
+                        "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, max_gif_lmt))
+                    if r.status_code == 200:
+                        # load the GIFs using the urls for the smaller GIF sizes
+                        top_8gifs = json.loads(r.content)
+                        if len(top_8gifs['results']) == 0:
+                            logging.info("IMGUR did not return any search terms")
+                            await message.channel.send("https://c.tenor.com/pQJqTGuF-FIAAAAC/simply-delicious.gif") 
+                            end_time = time.time()    
+                            logging.info("GIF Send Finished in: "+ str(end_time-start_time)+ " sec")   
+                            break 
+                        if len(top_8gifs['results']) > 1:
+                            random_gif_choice = random.randint(0,len(top_8gifs['results'])-1)
+                        else:
+                            random_gif_choice = 0
+                        
+                        gif_url = top_8gifs['results'][random_gif_choice]['media'][0]['gif']['url']
+                        end_time = time.time()    
+                        logging.info("Imjur Search Finished in: "+ str(end_time-start_time)+ " sec")
+                        start_time = end_time
+                        await message.channel.send(gif_url)
+                        end_time = time.time()    
+                        logging.info("GIF Send Finished in: "+ str(end_time-start_time)+ " sec")
+                        break
+                    break
+
+            else:
+                logging.info("NLP did not return any search terms")
+                await message.channel.send("https://c.tenor.com/pQJqTGuF-FIAAAAC/simply-delicious.gif") 
+                end_time = time.time()    
+                logging.info("GIF Send Finished in: "+ str(end_time-start_time)+ " sec")
+
+
+
+
+#discord API
+client.run(os.getenv("DISCORD_TOKEN"))
+
